6b211d753cfb5a4a93176c686bfcda4d045425b2
code3743 <jota.lopez@correounivalle.edu.co>
2024-12-18 10:49:49 -0500

    Refactor router to implement user authentication, teacher evaluation, and ratings endpoints

--- backend/src/router/router.ts
+++ backend/src/router/router.ts
@@ -1,19 +1,27 @@
 import { Router } from "express";
-import { Student } from "../models";
+import { signinController, updateUserController } from "../controllers/user.controller";
+import { getAllTeachers, getCurrentTeachers, getTeacherById} from "../controllers/teachers.controller";
+import { getRatingsByTeacherSubjectId, getRatingsSummary } from "../controllers/ratings.controller";
+import { getAllSubjects, getSubjectById } from "../controllers/subjects.controller";
+import { evaluateTeacher, getTeacherToEvaluate } from "../controllers/evaluate.teacher.controller";
+
 
 
 const router = Router();
 
-router.get("/", async (req, res) => {
-    const data = await Student.findAll()
-    res.json(data);
-});
-router.post('/signin', (req, res) => {});
-router.post('/signup', (req, res) => {});
-router.get('/current-teachers', (req, res) => {});
-router.post('/current-teachers/:id/evaluate', (req, res) => {});
-router.get('/current-teachers/:id/evaluate', (req, res) => {});
-router.get('/search', (req, res) => {});
+router.post('/login', signinController);
+router.post('/update', updateUserController);
+router.get('/current-teachers', getCurrentTeachers);
+router.get('/teachers', getAllTeachers);
+router.get('/teachers/:id', getTeacherById);
+router.get('/subjects', getAllSubjects);
+router.get('/subjects/:id', getSubjectById);
+router.get('/ratings', getRatingsSummary); 
+router.get('/ratings/:teacherSubjectId', getRatingsByTeacherSubjectId);
+router.get('/evaluate-teacher', getTeacherToEvaluate);
+router.post('/evaluate-teacher', evaluateTeacher)
+router.get('/search');
+
 
 
 
