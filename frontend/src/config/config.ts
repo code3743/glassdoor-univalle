36aeb881b051e686aa6c84ab831141b35bd78bcf
code3743 <jota.lopez@correounivalle.edu.co>
2024-12-18 21:41:35 -0500

    Add environment configuration and integrate Tailwind CSS with API endpoints

---
+++ frontend/src/config/config.ts
@@ -0,0 +1,19 @@
+export const API_BASE_URL = import.meta.env.API_BASE_URL;
+
+const ENDPOINTS = {
+  LOGIN: `${API_BASE_URL}/login`,
+  UPDATE_USER: `${API_BASE_URL}/update`,
+  CURRENT_TEACHERS: `${API_BASE_URL}/current-teachers`,
+  TEACHERS: `${API_BASE_URL}/teachers`,
+  TEACHER_BY_ID: (id: string) => `${API_BASE_URL}/teachers/${id}`,
+  SUBJECTS: `${API_BASE_URL}/subjects`,
+  SUBJECT_BY_ID: (id: string) => `${API_BASE_URL}/subjects/${id}`,
+  RATINGS: `${API_BASE_URL}/ratings`,
+  RATINGS_BY_TEACHER_SUBJECT_ID: (teacherSubjectId: number) => `${API_BASE_URL}/ratings/${teacherSubjectId}`,
+  EVALUATE_TEACHER: `${API_BASE_URL}/evaluate-teacher`,
+  SEARCH:(query: string) => `${API_BASE_URL}/search?query=${query}`,
+};
+
+export default ENDPOINTS;
+
+
