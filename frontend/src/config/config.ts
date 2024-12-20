93b3c01f6b766c4c9abfe223bc9352a2c9dcf289
code3743 <jota.lopez@correounivalle.edu.co>
2024-12-20 18:23:39 -0500

    Update API endpoints to include '/api' prefix for consistency

--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -1,19 +1,19 @@
 export const API_BASE_URL = import.meta.env.API_BASE_URL;
 
 const ENDPOINTS = {
-  LOGIN: `${API_BASE_URL}/login`,
-  LOGOUT: `${API_BASE_URL}/logout`,
-  USER: `${API_BASE_URL}/user`,
-  UPDATE_USER: `${API_BASE_URL}/update`,
-  CURRENT_TEACHERS: `${API_BASE_URL}/current-teachers`,
-  TEACHERS: `${API_BASE_URL}/teachers`,
-  TEACHER_BY_ID: (id: string) => `${API_BASE_URL}/teachers/${id}`,
-  SUBJECTS: `${API_BASE_URL}/subjects`,
-  SUBJECT_BY_ID: (id: string) => `${API_BASE_URL}/subjects/${id}`,
-  RATINGS: `${API_BASE_URL}/ratings`,
-  RATINGS_BY_TEACHER_SUBJECT_ID: (teacherSubjectId: number) => `${API_BASE_URL}/ratings/${teacherSubjectId}`,
-  EVALUATE_TEACHER: `${API_BASE_URL}/evaluate-teacher`,
-  SEARCH:(query: string) => `${API_BASE_URL}/search?query=${query}`,
+  LOGIN: `${API_BASE_URL}/api/login`,
+  LOGOUT: `${API_BASE_URL}/api/logout`,
+  USER: `${API_BASE_URL}/api/user`,
+  UPDATE_USER: `${API_BASE_URL}/api/update`,
+  CURRENT_TEACHERS: `${API_BASE_URL}/api/current-teachers`,
+  TEACHERS: `${API_BASE_URL}/api/teachers`,
+  TEACHER_BY_ID: (id: string) => `${API_BASE_URL}/api/teachers/${id}`,
+  SUBJECTS: `${API_BASE_URL}/api/subjects`,
+  SUBJECT_BY_ID: (id: string) => `${API_BASE_URL}/api/subjects/${id}`,
+  RATINGS: `${API_BASE_URL}/api/ratings`,
+  RATINGS_BY_TEACHER_SUBJECT_ID: (teacherSubjectId: number) => `${API_BASE_URL}/api/ratings/${teacherSubjectId}`,
+  EVALUATE_TEACHER: `${API_BASE_URL}/api/evaluate-teacher`,
+  SEARCH:(query: string) => `${API_BASE_URL}/api/search?query=${query}`,
 };
 
 export default ENDPOINTS;
