47a80ef4cadb76932131ae85849ef4a779d4a0d3
code3743 <jota.lopez@correounivalle.edu.co>
2024-12-21 18:29:19 -0500

    Fix deployment file extensions and correct service type in PostgreSQL configuration

--- deploy.sh
+++ deploy.sh
@@ -14,27 +14,27 @@ fi
 
 
 
-kubectl apply -f backend-deployment.yaml
+kubectl apply -f backend-deployment.yml
 if [ $? -ne 0 ]; then
-  echo "Error aplicando backend-deployment.yaml"
+  echo "Error aplicando backend-deployment.yml"
   exit 1
 fi
 
-kubectl apply -f frontend-deployment.yaml
+kubectl apply -f frontend-deployment.yml
 if [ $? -ne 0 ]; then
-  echo "Error aplicando frontend-deployment.yaml"
+  echo "Error aplicando frontend-deployment.yml"
   exit 1
 fi
 
-kubectl apply -f postgres-deployment.yaml
+kubectl apply -f postgres-deployment.yml
 if [ $? -ne 0 ]; then
-  echo "Error aplicando postgres-deployment.yaml"
+  echo "Error aplicando postgres-deployment.yml"
   exit 1
 fi
 
-kubectl apply -f postgres-pvc.yaml
+kubectl apply -f postgres-pvc.yml
 if [ $? -ne 0 ]; then
-  echo "Error aplicando postgres-pvc.yaml"
+  echo "Error aplicando postgres-pvc.yml"
   exit 1
 fi
 
